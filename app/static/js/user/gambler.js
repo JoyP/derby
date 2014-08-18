eac35cbb719168ff4e49c7973239ab1346f3de1e
Joy Pratt <joympratt@gmail.com>
2014-08-17 21:49:52 -0500

    added jade files and finished test data

--- app/static/js/user/gambler.js
+++ app/static/js/user/gambler.js
@@ -2,7 +2,28 @@
   'use strict';
 
   $(document).ready(function(){
+    $('.info').click(sellAsset);
   });
 
+  function sellAsset(){
+    var id        = $(this).closest('.gambler').attr('data-gambler-id'),
+        asset     = $(this).children('.name').text(),
+        type      = 'delete',
+        url       = '/gamblers/' + id + '/assets/' + asset;
+
+    $.ajax({url:url, type:type, dataType:'json', success:function(data){
+      var $gambler = $('.gambler[data-gambler-id='+data.id+']');
+      $gambler.find('.cash').text('$' + data.cash.toFixed(2));
+      var $asset = $gambler.find('.info').children('.name:contains('+data.name+')').closest('.asset');
+      $asset.fadeOut();
+      setTimeout(function(){$asset.remove();}, 1000);
+
+      if(data.isDivorced){
+        var $spouse = $gambler.find('.spouse');
+        $spouse.fadeOut();
+        setTimeout(function(){$spouse.remove();}, 1000);
+      }
+    }});
+  }
 })();
 
